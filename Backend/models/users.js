91ea704136ab428b6f1a5c1060bd35b8d114fa3d
srisailamkakurala <srisailamkakurala@gmail.com>
2024-04-16 21:06:26 +0530

    initial commit

--- Backend/models/users.js
+++ Backend/models/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/Notes4you');
+mongoose.connect(process.env.MONGO_URL);
 
 const userSchema = new mongoose.Schema({
     username: String,
